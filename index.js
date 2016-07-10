fc688960006b8d553c0bbc9bbc2f8874d6e9a5ca
skeller01 <losingmyhead@yahoo.com>
2016-07-10 14:48:29 -0400

    continue work on game

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ function mesSelect(){
 function mesController(choice){
 	if(choice.toLowerCase() === "a" && artifactMes===0){
 		artifactMes=1;
-		artifacts=1;
+		artifacts=artifacts+1;
 		gamePrompt(["Here, take this Myoin Horn, an ancient Mesnidian instrument.","Myoin Hord added to inventory"],mesSelect);
 	}else if(choice.toLowerCase() === "p" && mesPlanet===0){
 		mesPlanet=1;
@@ -185,7 +185,7 @@ function goToKiyturn(){
 		if(visitedKiy===0){
 			visitedKiy=1;
 			// artifacts=1;
-			gamePrompt(["You've arrived at Mesnides. As you land, a representative of the Mesnidian people is there to greet you.","Welcome, traveler, to Mesnides"],kiySelect);
+			gamePrompt(["You've arrived at Kiyturn. As you land, a representative of the Kiyturn people is there to greet you.","Hello, what brings you to Kiyturn? You're not here to cause trouble are you?"],kiySelect);
 		}else{
 			gamePrompt(["You've already been here!","Where to next?"],beginTravel);
 		}
@@ -201,11 +201,11 @@ function kiySelect(){
 function kiyController(choice){
 	if(choice.toLowerCase() === "a" && artifactKiy===0){
 		artifactKiy=1;
-		artifacts=1;
-		gamePrompt(["Here, take this Myoin Horn, an ancient Mesnidian instrument.","Myoin Hord added to inventory"],kiySelect);
+		artifacts=artifacts+1;
+		gamePrompt(["Here, take this Kiyturn Glass Bowl, a symbol of our civilization.","Kiyturn Glass Bowl added to inventory."],kiySelect);
 	}else if(choice.toLowerCase() === "p" && kiyPlanet===0){
 		kiyPlanet=1;
-		gamePrompt("Well, Laplides suffered from atomic war and has been uninhabited for centuries. You would do well to avoid it on your journey.",kiySelect);
+		gamePrompt("I'm sorry, but we do not leave our planet. The universe, to us, is a beautiful mystery.",kiySelect);
 	}else if(choice.toLowerCase() === "l"){
 		gamePrompt("Roger that Captain!",beginTravel);
 
@@ -216,6 +216,140 @@ function kiyController(choice){
 
 };
 
+//+++++++++++++++++++++++++++++++++++++++These are the story functions 
+//***********************************************Aenides
+//Aenides variables 
+var visitedAen= 0;
+// var artifactLsp=0;
+// var mesArtifact = 0; 
+// var lapPlanet = 0; 
+
+
+//Aenides Starter
+function goToAenlides(){
+		if(visitedAen===0){
+			visitedAen=1;
+			// artifacts=1;
+			gamePrompt("You discover upon arrival to Aenides that they are a hostile people. You attempt to land, but they begin to fire upon your S.R.S.V. and you are forced to retreat.",beginTravel);
+		}else{
+			gamePrompt(["You've already been here!","Where to next?"],beginTravel);
+		}
+};
+
+//+++++++++++++++++++++++++++++++++++++++These are the story functions 
+//***********************************************Cramuthea
+//Cramuthea variables 
+var visitedCram= 0;
+
+// var artifactLsp=0;
+// var mesArtifact = 0; 
+// var lapPlanet = 0; 
+
+
+//Cramuthea Starter
+function goToCramuthea(){
+		if(visitedCram===0){
+			visitedCram=1;
+			var vehicleFuel=vehicleFuel+500;
+
+			// artifacts=1;
+			gamePrompt("Cramuthea has been abandoned due to global environmental disaster, but there are remnants of the people that left. You are able to refuel your ship (+500 gallons) and read a beacon signal that tells you the Cramuthean people have migrated to Smeon T9Q.",beginTravel);
+		}else{
+			gamePrompt(["You've already been here!","Where to next?"],beginTravel);
+		}
+};
+
+//+++++++++++++++++++++++++++++++++++++++These are the story functions 
+//***********************************************smeon
+//Smeon variables 
+var visitedSmeon = 0;
+var artifactSmeon=0;
+var SmeonArtifact = 0; 
+var SmeonPlanet = 0; 
+
+
+//Smeon Starter
+function goToSmeon(){
+		if(visitedSmeon===0){
+			visitedSmeon=1;
+			vehicleFuel=vehicleFuel+100;
+			// artifacts=1;
+			gamePrompt("The Cramuthean people, living on Smeon T9Q, are a friendly people that give you some fuel (+100 gallons) when you arrive.",SmeonSelect);
+		}else{
+			gamePrompt(["You've already been here!","Where to next?"],beginTravel);
+		}
+};
+
+ 
+//Smeon controller
+function SmeonSelect(){
+	gamePrompt(["How can we assist you?","Ask about (A)rtifact. Ask about other (P)lanets. (L)eave"],smeonController);
+};
+
+//main Smeon controller
+function SmeonController(choice){
+	if(choice.toLowerCase() === "a" && artifactSmeon===0){
+		artifactSmeon=1;
+		artifacts=artifacts+1;
+		gamePrompt(["they also give you a dried Cramun Flower from their home planet.","Cramun Flower added to inventory."],SmeonSelect);
+	}else if(choice.toLowerCase() === "p" && SmeonPlanet===0){
+		SmeonPlanet=1;
+		gamePrompt("they warn you that the people of Aenides once tried to take over their home planet by force.",SmeonSelect);
+	}else if(choice.toLowerCase() === "l"){
+		gamePrompt("Roger that Captain!",beginTravel);
+
+	}else{
+		gamePrompt("Sorry Captain, we didn't understand your input or you have done this already. Please reselect",SmeonSelect);
+	}
+
+
+};
+
+//+++++++++++++++++++++++++++++++++++++++These are the story functions 
+//***********************************************Gleshan
+//Gleshan variables 
+var visitedSmeon = 0;
+var artifactSmeon=0;
+var SmeonArtifact = 0; 
+var SmeonPlanet = 0; 
+
+
+//Gleshan Starter
+function goToSmeon(){
+		if(visitedSmeon===0){
+			visitedSmeon=1;
+			vehicleFuel=vehicleFuel+100;
+			// artifacts=1;
+			gamePrompt("The Cramuthean people, living on Smeon T9Q, are a friendly people that give you some fuel (+100 gallons) when you arrive.",SmeonSelect);
+		}else{
+			gamePrompt(["You've already been here!","Where to next?"],beginTravel);
+		}
+};
+
+ 
+//Gleshan controller
+function SmeonSelect(){
+	gamePrompt(["How can we assist you?","Ask about (A)rtifact. Ask about other (P)lanets. (L)eave"],smeonController);
+};
+
+//main Gleshan controller
+function SmeonController(choice){
+	if(choice.toLowerCase() === "a" && artifactSmeon===0){
+		artifactSmeon=1;
+		artifacts=artifacts+1;
+		gamePrompt(["they also give you a dried Cramun Flower from their home planet.","Cramun Flower added to inventory."],SmeonSelect);
+	}else if(choice.toLowerCase() === "p" && SmeonPlanet===0){
+		SmeonPlanet=1;
+		gamePrompt("they warn you that the people of Aenides once tried to take over their home planet by force.",SmeonSelect);
+	}else if(choice.toLowerCase() === "l"){
+		gamePrompt("Roger that Captain!",beginTravel);
+
+	}else{
+		gamePrompt("Sorry Captain, we didn't understand your input or you have done this already. Please reselect",SmeonSelect);
+	}
+
+
+};
 
 //call a function so we can begin the game 
 startGame();
